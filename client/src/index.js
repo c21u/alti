c288ae7538961bf9c95eae09cf5845e1c2581ab9
Emily Reese <ereese3@mail.gatech.edu>
2018-11-19 15:49:02 -0500

    Refactor client index.

--- client/src/index.js
+++ client/src/index.js
@@ -1,62 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import theme from "@instructure/ui-themes/lib/canvas";
-import jwtDecode from "jwt-decode";
-import qs from "qs";
+import { Provider } from "mobx-react";
 import App from "./App";
-import CommonContext from "./context";
+import CommonStore from "./stores/CommonStore";
 
 theme.use();
 
-/**
- * Look for a JWT in location query parameters.
- * @return {?string}
- */
-function grabJWT() {
-  try {
-    let jwt = qs.parse(window.location.search, { ignoreQueryPrefix: true })
-      .token;
-    jwtDecode(jwt);
-    return jwt;
-  } catch (reason) {
-    console.error(reason);
-  }
-  return null;
-}
+const stores = { CommonStore };
 
-/**
- * App container.
- */
-class AppContainer extends React.Component {
-  /**
-   * @constructor
-   * @param {Object} props
-   */
-  constructor(props) {
-    super(props);
-    this.state = {
-      jwt: grabJWT()
-    };
-  }
-
-  /**
-   * @return {Component}
-   */
-  render() {
-    return (
-      <div>
-        {this.state.jwt ? (
-          <CommonContext.Provider value={this.state.jwt}>
-            <App />
-          </CommonContext.Provider>
-        ) : (
-          <p>
-            TODO: error message instead of the app, because getting JWT failed.
-          </p>
-        )}
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<AppContainer />, document.getElementById("lti_root"));
+ReactDOM.render(
+  <Provider {...stores}>
+    <App />
+  </Provider>,
+  document.getElementById("lti_root")
+);
