0254325d39fc8ad156bc64cd4b9d344325db409a
Emily Reese <ereese3@mail.gatech.edu>
2018-11-21 09:50:33 -0500

    Refactor server tests.
    Run tests in a way that Jest expects.

--- server.test.js
+++ server.test.js
@@ -8,55 +8,42 @@ const app = require("./server");
 const fakeAuth = require("./config")["fakeStrategyCredentials"];
 
 test("Can POST to be authenticated", () => {
+  expect.assertions(2);
+
   // Expect the fake credentials to exist.
   expect(fakeAuth.username).toBeDefined();
   expect(fakeAuth.password).toBeDefined();
 
-  request(app)
+  return request(app)
     .post("/")
     .send(`username=${fakeAuth.username}`)
     .send(`password=${fakeAuth.password}`)
     .expect(302)
-    .expect("Location", /^\/\?token=.+\..+\..+$/)
-    .end((err, res) => {
-      if (err) throw err;
-    });
+    .expect("Location", /^\/\?token=.+\..+\..+$/);
 });
 
 test("Should not authenticate with bad credentials", () => {
-  request(app)
+  return request(app)
     .post("/")
     .send(`username=incorrect`)
     .send(`password=password`)
-    .expect(401)
-    .end((err, res) => {
-      if (err) throw err;
-    });
+    .expect(401);
 });
 
 test("GET / should be protected", () => {
-  request(app)
+  return request(app)
     .get("/")
-    .expect(401)
-    .end((err, res) => {
-      if (err) throw err;
-    });
+    .expect(401);
 });
 
 test("/api routes should be protected", () => {
-  request(app)
+  return request(app)
     .get("/api")
-    .expect(401)
-    .end((err, res) => {
-      if (err) throw err;
-    });
+    .expect(401);
 });
 
 test("Health check returns 200", () => {
-  request(app)
+  return request(app)
     .get("/z")
-    .expect(200)
-    .end((err, res) => {
-      if (err) throw err;
-    });
+    .expect(200);
 });
