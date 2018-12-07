7be9d0969e1da8d385da80d7a87d3a3e8ad28ec4
Emily Reese <ereese3@mail.gatech.edu>
2018-12-07 17:02:40 -0500

    webpack: resolve js or jsx files; rename entry.

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,7 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: "./client/src/index.js"
+    app: "./client/src/index.jsx"
   },
   output: {
     filename: "[name].bundle.js",
@@ -14,6 +14,7 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
+        resolve: { extensions: [".js", ".jsx"] },
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
