4641a60477439e359b04b47643a055ecd153603b
Emily Reese <ereese3@mail.gatech.edu>
2018-12-07 16:56:14 -0500

    refactor: Move components into a folder.

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import theme from "@instructure/ui-themes/lib/canvas";
 import { Provider } from "mobx-react";
-import App from "./App";
+import App from "./components/App";
 import CommonStore from "./stores/CommonStore";
 
 theme.use();
