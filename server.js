9954a84c39c407a457b200c6a0d21f39f3f7c731
Emily Reese <ereese3@mail.gatech.edu>
2019-01-08 09:29:18 -0500

    Raise default payload size limit.

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ if (sentryDSN) {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: false, limit: "10mb" }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "dist")));
 
