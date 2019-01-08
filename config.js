a15a2fb5daa572eceebc8fcab27882bcc8b579df
Emily Reese <ereese3@mail.gatech.edu>
2019-01-08 11:15:27 -0500

    refactor: Update config.
    Remove getEnvVarOrThrow behavior.
    Stop logging default values with console.warn.
    Remove try/catch block.
    Change some config setting names and organization.
    Remove httpLogs configs.
    Add logLevel configs.

--- config.js
+++ config.js
@@ -5,7 +5,6 @@ const getEnvVarOrDefault = (envVar, defaultValue) => {
   if (!!process.env[envVar]) {
     return process.env[envVar];
   } else {
-    console.warn(`${envVar} not set: using default ${defaultValue}`);
     return defaultValue;
   }
 };
@@ -18,48 +17,39 @@ const getEnvVarOrNull = envVar => {
   }
 };
 
-const getEnvVarOrThrow = envVar => {
-  if (!!process.env[envVar]) {
-    return process.env[envVar];
-  } else {
-    throw new Error(`Unrecoverable: missing process.env.${envVar}`);
-  }
-};
-
 const config = {};
-try {
-  config.buzzAPI = {
-    appID: getEnvVarOrDefault("BUZZAPI_APP_ID"),
-    password: getEnvVarOrDefault("BUZZAPI_PASSWORD")
-  };
-  config.canvasApiUrl = getEnvVarOrDefault("CANVAS_API_URL");
-  config.canvasToken = getEnvVarOrDefault("CANVAS_TOKEN");
-  config.fakeStrategyCredentials = {};
-  config.httpLogsFormat = "combined";
-  config.googleAnalyticsID = getEnvVarOrNull("GOOGLE_ANALYTICS_ID");
-  config.jwtSecret = getEnvVarOrDefault("JWT_SECRET");
-  config.lti = {
-    key: getEnvVarOrThrow("LTI_KEY"),
-    secret: getEnvVarOrThrow("LTI_SECRET")
-  };
-  config.passportStrategy = "lti";
-  config.sentryDSN = getEnvVarOrNull("SENTRY_DSN");
-  config.trustProxy = getEnvVarOrDefault("TRUST_PROXY", "loopback");
-} catch (err) {
-  console.error(err);
-}
+/* Set default values assuming NODE_ENV === production */
+config.buzzAPI = {
+  appID: getEnvVarOrDefault("BUZZAPI_APP_ID"),
+  password: getEnvVarOrDefault("BUZZAPI_PASSWORD")
+};
+config.canvas = {
+  apiUrl: getEnvVarOrDefault("CANVAS_API_URL"),
+  token: getEnvVarOrDefault("CANVAS_TOKEN")
+};
+config.fakeStrategyCredentials = {};
+config.googleAnalyticsID = getEnvVarOrNull("GOOGLE_ANALYTICS_ID");
+config.jwtSecret = getEnvVarOrDefault("JWT_SECRET");
+config.lti = {
+  key: getEnvVarOrDefault("LTI_KEY"),
+  secret: getEnvVarOrDefault("LTI_SECRET")
+};
+config.logLevel = getEnvVarOrDefault("LOG_LEVEL", "info");
+config.passportStrategy = "lti";
+config.sentryDSN = getEnvVarOrNull("SENTRY_DSN");
+config.trustProxy = getEnvVarOrDefault("TRUST_PROXY", "loopback");
 
 if (process.env.NODE_ENV === "development") {
-  config["httpLogsFormat"] = "dev";
+  config.logLevel = "debug";
 }
 
 if (process.env.NODE_ENV === "test") {
-  console.warn(`Fake auth strategy enabled!`);
-  config["fakeStrategyCredentials"] = {
+  config.fakeStrategyCredentials = {
     username: getEnvVarOrDefault("FAKE_USERNAME"),
     password: getEnvVarOrDefault("FAKE_PASSWORD")
   };
-  config["passportStrategy"] = "fake";
+  config.logLevel = "error";
+  config.passportStrategy = "fake";
 }
 
 module.exports = config;
