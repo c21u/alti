bdc08ba1b89294ebec1fcf545ab060d8496b1d76
Emily Reese <ereese3@mail.gatech.edu>
2019-01-09 09:18:27 -0500

    refactor: Update tests.

--- server.test.js
+++ server.test.js
@@ -2,42 +2,64 @@
  * @jest-environment node
  */
 
+const jwtDecode = require("jwt-decode");
 const request = require("supertest");
 
 const app = require("./server");
 const fakeAuth = require("./config")["fakeStrategyCredentials"];
+const parseQueryParameters = require("./lib/util").parseQueryParameters;
 
-test("Can POST to be authenticated", () => {
-  expect.assertions(2);
-
-  // Expect the fake credentials to exist.
-  expect(fakeAuth.username).toBeDefined();
-  expect(fakeAuth.password).toBeDefined();
-
+/**
+ * Utility function for Passport LTI auth step.
+ * @return {Promise}
+ */
+const getFakeAuthenticated = () => {
   return request(app)
     .post("/")
     .send(`username=${fakeAuth.username}`)
     .send(`password=${fakeAuth.password}`)
-    .expect(302)
-    .expect("Location", /^\/\?.*token=.+\..+\..+$/);
+    .set("Accept", "applicaton/json");
+};
+
+describe("Authentication via Passport middleware", () => {
+  test("Should get successful Passport authentication.", () => {
+    expect.assertions(4);
+
+    // Expect the fake credentials to exist.
+    expect(fakeAuth.username).toBeDefined();
+    expect(fakeAuth.password).toBeDefined();
+
+    return getFakeAuthenticated()
+      .expect(302)
+      .expect("Cache-Control", "no-store")
+      .expect("Pragma", "no-cache")
+      .then(response => {
+        const queryParameters = parseQueryParameters(response.header);
+        expect(queryParameters.token).toBeDefined();
+        expect(() => {
+          jwtDecode(queryParameters.token);
+        }).not.toThrow();
+      });
+  });
 });
 
-test("Should not authenticate with bad credentials", () => {
-  return request(app)
-    .post("/")
-    .send(`username=incorrect`)
-    .send(`password=password`)
-    .expect(401);
-});
+describe("Unauthenticated server routes", () => {
+  test("Health check returns 200", () => {
+    return request(app)
+      .get("/z")
+      .expect(200);
+  });
+  test("Should not authenticate with bad credentials", () => {
+    return request(app)
+      .post("/")
+      .send(`username=incorrect`)
+      .send(`password=password`)
+      .expect(401);
+  });
 
-test("/api routes should be protected", () => {
-  return request(app)
-    .get("/api")
-    .expect(401);
-});
-
-test("Health check returns 200", () => {
-  return request(app)
-    .get("/z")
-    .expect(200);
+  test("/api route should be protected", () => {
+    return request(app)
+      .get("/api")
+      .expect(401);
+  });
 });
