f3cd72c3010b742c56ae2844971e5230d17206bc
Emily Reese <ereese3@mail.gatech.edu>
2019-02-19 08:56:25 -0500

    Update and expand API tests.

--- routes/canvasStatusHandler.test.js
+++ routes/canvasStatusHandler.test.js
@@ -2,21 +2,54 @@
  * @jest-environment node
  */
 
-describe("API", () => {
-  const FakeData = {
-    getAccounts: Promise.resolve({
-      body: "anything"
-    })
-  };
+describe("API routes", () => {
+  describe("/api/canvas-status", () => {
+    test("Should get a successful response.", () => {
+      const FakeDataLayer = {
+        getAccounts: Promise.resolve({
+          body: "anything"
+        })
+      };
+      const canvasStatusHandler = require("./canvasStatusHandler")(
+        FakeDataLayer
+      );
+      const fakeRequest = {};
+      return canvasStatusHandler(fakeRequest).then(response => {
+        expect(response).toBeDefined();
+        expect(response.status).toBeDefined();
+        expect(response.status).toBe("success");
+      });
+    });
 
-  const fakeRequest = {};
-
-  test("#Canvas-status", () => {
-    const canvasStatusHandler = require("./canvasStatusHandler")(FakeData);
-    return canvasStatusHandler(fakeRequest).then(response => {
-      expect(response).toBeDefined();
-      expect(response.status).toBeDefined();
-      expect(response.status).toBe("success");
+    test("Should get 'error' if response body missing.", () => {
+      const FakeDataLayerNoBody = {
+        getAccounts: Promise.resolve({})
+      };
+      const canvasStatusHandler = require("./canvasStatusHandler")(
+        FakeDataLayerNoBody
+      );
+      const fakeRequest = {};
+      return canvasStatusHandler(fakeRequest).then(response => {
+        expect(response).toBeDefined();
+        expect(response.status).toBeDefined();
+        expect(response.status).toBe("error");
+      });
+    });
+    test("Should get 'error' when data layer fails.", () => {
+      const FakeDataLayerReject = {
+        getAccounts: Promise.reject(new Error("REJECTEDPROMISE"))
+      };
+      const canvasStatusHandler = require("./canvasStatusHandler")(
+        FakeDataLayerReject
+      );
+      const fakeRequest = {};
+      return canvasStatusHandler(fakeRequest).then(response => {
+        expect(response).toBeDefined();
+        expect(response.status).toBeDefined();
+        expect(response.status).toBe("error");
+        expect(response.status).toBeDefined();
+        expect(response.message).toBe("REJECTEDPROMISE");
+      });
     });
   });
 });
